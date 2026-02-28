64286cf4634fa8eb66c186e74ad1a8263afdeda0
Kmc <kmcgurty1@gmail.com>
2026-02-28 13:32:01 -0500

    Initial commit

---
+++ src/routes/[...slug]/+page.svelte
@@ -0,0 +1,11 @@
+<div class="text-center">
+	<h1>{window.location.hostname} is blocked</h1>
+	<p>
+		Click here to allow access for
+		<a href="/">1hr</a>,
+		<a href="/">24hr</a>,
+		<a href="/">permanently</a>.
+	</p>
+    <br />
+    <div class="text-sm">{window.location.href}</div>
+</div>
